28d096d738e2f99eb77117794f83e160fa054515
Sikyong <iyh99185@naver.com>
2017-01-21 00:10:50 +0900

    Splash Activity

---
+++ app/src/main/java/kr/ac/mju/hanmaeum/activity/Splash.java
@@ -0,0 +1,15 @@
+package kr.ac.mju.hanmaeum.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.ac.mju.hanmaeum.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
