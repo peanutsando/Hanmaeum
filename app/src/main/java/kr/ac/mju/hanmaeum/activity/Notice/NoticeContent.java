41e32b3a69bd7d4909bab2ba4deb2b4bb8b37f7a
Jinhyeon Park <wlsgussla123@gmail.com>
2017-01-20 11:19:23 +0900

    notice content layout

---
+++ app/src/main/java/kr/ac/mju/hanmaeum/activity/Notice/NoticeContent.java
@@ -0,0 +1,10 @@
+package kr.ac.mju.hanmaeum.activity.Notice;
+
+import kr.ac.mju.hanmaeum.activity.BaseActivity;
+
+/**
+ * Created by user on 2017-01-19.
+ */
+
+public class NoticeContent extends BaseActivity {
+}
