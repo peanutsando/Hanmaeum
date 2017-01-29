9741d1c21ffddc71b12366aec9c9526c95934d7f
Sikyong <iyh99185@naver.com>
2017-01-29 21:15:53 +0900

    update Shuttles_radioButton_onclickevent

--- app/src/main/java/kr/ac/mju/hanmaeum/fragment/ShuttleFragment.java
+++ app/src/main/java/kr/ac/mju/hanmaeum/fragment/ShuttleFragment.java
@@ -7,8 +7,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+
 import android.widget.ListView;
+import android.widget.RadioButton;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -29,6 +30,10 @@ public class ShuttleFragment extends Fragment {
     private ArrayList<Shuttle> shuttleList;
     @BindView(R.id.shuttleTime)
     ListView shuttleTime;
+    public RadioButton rb1;
+    public RadioButton rb2;
+    public RadioButton rb3;
+
 
     public ShuttleFragment() {
         // Required empty public constructor
@@ -53,9 +58,19 @@ public class ShuttleFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_shuttle, container, false);
         ButterKnife.bind(this, view);
 
+        rb1=(RadioButton)view.findViewById(R.id.shuttle_rb1);
+        rb2=(RadioButton)view.findViewById(R.id.shuttle_rb2);
+        rb3=(RadioButton)view.findViewById(R.id.shuttle_rb3);
+
         shuttleList = new ArrayList<Shuttle>();
         GetShuttleTime getShuttleTime = new GetShuttleTime();
         getShuttleTime.execute();
+
+        rb1.setOnClickListener(optionClickListener);
+        rb2.setOnClickListener(optionClickListener);
+        rb3.setOnClickListener(optionClickListener);
+
+        rb1.setChecked(true);
 
         return view;
     }
@@ -63,6 +78,7 @@ public class ShuttleFragment extends Fragment {
     class GetShuttleTime extends AsyncTask<Void, Void, ArrayList<Shuttle>> {
 
         // 처리하는 곳
+        // 셔틀정보를가져오는 부분.
         @Override protected ArrayList<Shuttle> doInBackground(Void... voids) {
             try {
                 Document doc = Jsoup.connect(Constants.SHUTTLE_URL).get();
@@ -74,6 +90,7 @@ public class ShuttleFragment extends Fragment {
                     boolean flag = isNumber(split[0]);
 
                     if (flag) {
+
                         Log.i("TAG", e.text());
                         if (split.length == 3) {
                             shuttleList.add(new Shuttle(split[0], getString(R.string.downtownShuttle), split[1], split[2]));
@@ -86,7 +103,6 @@ public class ShuttleFragment extends Fragment {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
             return null;
         }
 
@@ -101,6 +117,35 @@ public class ShuttleFragment extends Fragment {
             super.onPostExecute(shuttleList);
         }
     }
+
+    RadioButton.OnClickListener optionClickListener = new RadioButton.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            ArrayList<Shuttle> checkShuttleList = new ArrayList<>();
+            if(v.getId() == R.id.shuttle_rb1){
+                for(int i=0; i<75; i++){
+                    checkShuttleList.add(shuttleList.get(i));
+                    Log.i("TAG", checkShuttleList.get(i).toString());
+                }
+                ShuttleAdapter shuttleAdapter = new ShuttleAdapter(getActivity(), checkShuttleList);
+                shuttleTime.setAdapter(shuttleAdapter);
+            }
+            else if(v.getId() == R.id.shuttle_rb2){
+                for(int i=0;i<65;i++){
+                    checkShuttleList.add(shuttleList.get(i));
+                }
+                ShuttleAdapter shuttleAdapter = new ShuttleAdapter(getActivity(), checkShuttleList);
+                shuttleTime.setAdapter(shuttleAdapter);
+            }
+            else if(v.getId()==R.id.shuttle_rb3){
+                for(int i=75;i<85;i++){
+                    checkShuttleList.add(shuttleList.get(i));
+                }
+                ShuttleAdapter shuttleAdapter = new ShuttleAdapter(getActivity(), checkShuttleList);
+                shuttleTime.setAdapter(shuttleAdapter);
+            }
+        }
+    };
 
     private boolean isNumber(String str) {
         try {
