6b7403646fdddf691fee310bf7261d7073a75335
Jinhyeon Park <wlsgussla123@gmail.com>
2017-02-07 20:15:49 +0900

    recovery shuttle Fragment

--- app/src/main/java/kr/ac/mju/hanmaeum/fragment/ShuttleFragment.java
+++ app/src/main/java/kr/ac/mju/hanmaeum/fragment/ShuttleFragment.java
@@ -78,12 +78,6 @@ public class ShuttleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        BackRunnable runnable = new BackRunnable();
-        Thread thread = new Thread(runnable);
-        thread.setDaemon(true);
-        thread.start();
     }
 
     @Override
@@ -309,61 +303,6 @@ public class ShuttleFragment extends Fragment {
             }
 
             PreferenceManager.setShuttleDatabase(getActivity());
-        }
-    }
-
-    class BackRunnable implements Runnable {
-        public void run() {
-            while(true) {
-                if(ShuttleService.getShuttleServiceCheckList(getActivity())) {
-                    sendNotification();
-                }
-                try {
-                    Thread.sleep(60000);
-                }catch(InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        private void sendNotification() {
-            RequestBody body = new FormBody.Builder()
-                    .add("Message", "버스 도착 5분 전")
-                    .build();
-
-            //request
-            Request request = new Request.Builder()
-                    .url("http://183.101.80.77:880/FCM/push_notification.php")
-                    .post(body)
-                    .build();
-
-            client = new OkHttpClient.Builder()
-                    .connectTimeout(60, TimeUnit.SECONDS)
-                    .writeTimeout(20, TimeUnit.SECONDS)
-                    .readTimeout(60, TimeUnit.SECONDS)
-                    .build();
-
-            okhttp3.Call call = client.newCall(request);
-            call.enqueue(new okhttp3.Callback() {
-                @Override
-                public void onFailure(okhttp3.Call call, IOException e) {
-                    Log.e("ONFAILURE@@@@@@@", "Registration error: " + e.getMessage());
-                }
-
-                @Override
-                public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                    try {
-                        String resp = response.body().string();
-                        Log.v("ONSREPONSE!!!!", resp);
-                        if (response.isSuccessful()) {
-                        } else {
-
-                        }
-                    } catch (IOException e) {
-                        // Log.e(TAG_REGISTER, "Exception caught: ", e);
-                    }
-                }
-            });
         }
     }
 }
